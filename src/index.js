229206cd666c4b7748490831a027657bd304fabd
Salypse <84153483+Salypse@users.noreply.github.com>
2025-11-07 14:17:29 -0600

    Return weather data of users inputed location name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import "./styles.css"
 import { getLocationData } from "../src/js/weather-data"
 
-/*
-const data = await getLocationData("Chicago")
-console.log(data)
-*/
+async function contentHandler() {
+    const locationForm = document.getElementById("location-form")
+    const locationInput = document.getElementById("location-input")
+
+    locationForm.addEventListener("submit", async event => {
+        event.preventDefault()
+
+        try {
+            const response = await getLocationData(locationInput.value)
+            console.log(response)
+        } catch (error) {
+            console.error(`Error submitting form: ${error}`)
+        }
+        
+    })
+};
+
+
+contentHandler()
